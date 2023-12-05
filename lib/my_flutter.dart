0a9e403334c0fa6abed4316daa00bd87d2182f6b
luoyi <luoyi58624@163.com>
2023-12-05 18:07:55 +0800

    export flustars

--- lib/my_flutter.dart
+++ lib/my_flutter.dart
@@ -1,3 +1,13 @@
 library my_flutter;
 
+import 'package:flustars/flustars.dart';
+
+export 'package:flustars/flustars.dart';
+
 export './utils/logger.dart';
+export './utils/common.dart';
+export './utils/toast.dart';
+
+Future<void> initMyFlutter() async {
+  await SpUtil.getInstance();
+}
