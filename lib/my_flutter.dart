759cabe1dc19ed8e03ae6ffc99bba1fbc4c7dd29
luoyi <luoyi58624@163.com>
2023-12-05 18:11:38 +0800

    export get、go_router

--- lib/my_flutter.dart
+++ lib/my_flutter.dart
@@ -3,6 +3,8 @@ library my_flutter;
 import 'package:flustars/flustars.dart';
 
 export 'package:flustars/flustars.dart';
+export 'package:get/get.dart';
+export 'package:go_router/go_router.dart';
 
 export './utils/logger.dart';
 export './utils/common.dart';
