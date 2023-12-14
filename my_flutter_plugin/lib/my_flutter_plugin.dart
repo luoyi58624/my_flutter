12d5217fb6ff3b3a16dc33116e2f4ed804c640c6
luoyi <luoyi58624@163.com>
2023-12-14 14:33:15 +0800

    export flutter_localizations

--- my_flutter_plugin/lib/my_flutter_plugin.dart
+++ my_flutter_plugin/lib/my_flutter_plugin.dart
@@ -1,7 +1,3 @@
 library my_flutter_plugin;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:flutter_localizations/flutter_localizations.dart';
