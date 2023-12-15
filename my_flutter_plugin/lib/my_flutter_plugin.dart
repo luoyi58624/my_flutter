74205122ff29b10a347a56fc60aeb838c994f1e9
luoyi <luoyi58624@163.com>
2023-12-15 14:58:38 +0800

    get的路由功能没有go_router多，所以只导出get的状态管理，排除路由、网络请求等库

--- my_flutter_plugin/lib/my_flutter_plugin.dart
+++ my_flutter_plugin/lib/my_flutter_plugin.dart
@@ -1,12 +1,10 @@
 library my_flutter_plugin;
 
 export 'package:flutter_localizations/flutter_localizations.dart';
-export 'package:get/get.dart' hide Response;
 
-export 'package:get/get_navigation/src/router_report.dart';
 // 仅导出getx状态管理，排除路由等其他功能
-// export 'package:get/get_core/get_core.dart';
-// export 'package:get/get_instance/get_instance.dart';
-// export 'package:get/get_state_manager/get_state_manager.dart';
-// export 'package:get/get_utils/get_utils.dart';
-// export 'package:get/get_rx/get_rx.dart';
+export 'package:get/get_core/get_core.dart';
+export 'package:get/get_instance/get_instance.dart';
+export 'package:get/get_state_manager/get_state_manager.dart';
+export 'package:get/get_utils/get_utils.dart';
+export 'package:get/get_rx/get_rx.dart';
