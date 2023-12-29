f97fc90b0326a7049cd100b938503df55f54a104
luoyi <luoyi58624@163.com>
2023-12-29 11:05:27 +0800

    导出modal弹窗工具类

--- lib/util/index.dart
+++ lib/util/index.dart
@@ -2,6 +2,7 @@ export 'color.dart';
 export 'common.dart';
 export 'local_storage.dart';
 export 'logger.dart';
+export 'modal.dart';
 export 'no_ripper.dart';
 export 'router.dart';
 export 'loading.dart';
